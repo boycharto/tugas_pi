6f0db738e6d83c623dc7bbd0d16c9b6371930ab7
boycharto <66813890+boycharto@users.noreply.github.com>
2021-04-03 09:44:02 +0700

    rancangan formulir awal

---
+++ index.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+	<title>Tugas PI</title>
+	</head>
+
+<body>
+	<div class="body_site">
+		<header>
+			<div class="banner clearfix">
+         <h1>Tugas PI Form</h1>
+      </div>
+		</header>
+
+		<main>
+			<div class="main_site clearfix">
+				<h2>Crud </h2>
+			
+					<form class="page1_form" action="page2.php">
+						<ul>
+							<li>
+						<div class="user_info">
+							<label>Firstname:</label>
+							<input type  = "text" id = "firstname" required>
+						</div></li>
+						<li>
+						<div class="user_info">
+							<label>Lastname:</label>
+							<input type  = "text" id = "lastname" required>
+						</div></li>
+						<li>
+						<div class="user_info">
+							<label>Contact Number:</label>
+							<input type  = "text" id = "contactnum" required>
+						</div></li>
+						<div>
+							<button class="btn_reg" type = "submit" value="submit" id="addnew">Register</button>
+						</div>
+					</form>
+				</div>
+		</main>
+
+		<footer>
+			
+			</div>
+		</footer>
+
+	</div>
+</body>
+
+
+	</script>
+
+</html>
